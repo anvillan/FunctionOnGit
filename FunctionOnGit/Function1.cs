24c73d851bd70c522daab526405c4b8a17a155ad
anvillan <anvillan@users.noreply.github.com>
2020-10-01 10:52:22 -0700

    Test new template Win - PackagePath

--- FunctionOnGit/Function1.cs
+++ FunctionOnGit/Function1.cs
@@ -12,7 +12,7 @@ namespace FunctionOnGit
 {
     public static class Function1
     {
-        [FunctionName("FunctionOnGitWindowsNewLinux")]
+        [FunctionName("FunctionOnGitPublishedActions")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
